d04309c0a739b0c9c5baf5cfbf54848853d6c746
Annatar999 <96719369+Annatar999@users.noreply.github.com>
2022-02-02 23:52:55 +0500

    Add files via upload

---
+++ package/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='itea',
+    version='0.1',
+    auther='NoName',
+    auther_email='noname@gmail.com',
+    packages=['itea'],
+    python_requires='>=3.5',
+)
